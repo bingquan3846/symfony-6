7cd60c15478cb9d10735bc1e023c05e68ecf0d71
bingquan bao <bingquan3846@gmail.com>
2023-03-21 11:43:20 +0100

    use twig

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Conference;
+use App\Repository\CommentRepository;
+use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,15 +12,19 @@ use Symfony\Component\Routing\Annotation\Route;
 class ConferenceController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function index(ConferenceRepository $conferenceRepository): Response
     {
-        return new Response(<<<EOF
-            <html>
-                <body>
-                    <img src="/images/under-construction.gif" />
-                </body>
-            </html>
-        EOF
-        );
+        return $this->render('conference/index.html.twig', [
+            'conferences' => $conferenceRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/conference/{id}', name: 'conference')]
+    public function show(Conference $conference, CommentRepository $commentRepository) : Response
+    {
+        return $this->render('conference/show.html.twig', [
+            'conference' => $conference,
+            'comments' => $commentRepository->findBy(['conference' => $conference], ['createdAt' => 'DESC']),
+        ]);
     }
 }
